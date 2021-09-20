b8c155eefb436b73bbca294a25f5f6468485b316
Bartłomiej <bartlomiej.kotarski@netguru.com>
2021-09-20 22:38:56 +0200

    :sparkles: add rabbitmq compose with wait timeout

--- producer/src/index.js
+++ producer/src/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const amqp = require('amqplib');
 
 const app = express();
 
 
 const PORT = process.env.PORT;
+const RABBIT_HOST = process.env.RABBIT_HOST
+
 
 app.get("/", (req, res) => {
     res.send("Hello me").status(200)
 })
 
-app.listen(PORT, () => {
-    console.log(`App started on port ${PORT}`)
-})
+async function main() {
+    const connection = await amqp.connect(`amqp://${RABBIT_HOST}`)
+
+    const channel = await connection.createChannel();
+
+    app.listen(PORT, () => {
+        console.log(`App started on port ${PORT}`)
+    })
+}
+
+main()
