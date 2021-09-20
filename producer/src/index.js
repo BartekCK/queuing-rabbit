a8d85ea93f8801f507949510951b6b006cf92042
Bartłomiej <bartlomiej.kotarski@netguru.com>
2021-09-20 22:58:48 +0200

    :sparkles: send msg to rabbit

--- producer/src/index.js
+++ producer/src/index.js
@@ -2,20 +2,25 @@ const express = require("express");
 const amqp = require('amqplib');
 
 const app = express();
-
+app.use(express.json());
 
 const PORT = process.env.PORT;
 const RABBIT_HOST = process.env.RABBIT_HOST
-
-
-app.get("/", (req, res) => {
-    res.send("Hello me").status(200)
-})
+const QUEUE_NAME = process.env.QUEUE_NAME
 
 async function main() {
     const connection = await amqp.connect(`amqp://${RABBIT_HOST}`)
-
     const channel = await connection.createChannel();
+
+    channel.assertQueue(QUEUE_NAME, {
+        durable: false
+    });
+
+    app.post("/", (req, res) => {
+        const {message} = req.body;
+        channel.sendToQueue(QUEUE_NAME, Buffer.from(message));
+        res.status(201).send(message)
+    })
 
     app.listen(PORT, () => {
         console.log(`App started on port ${PORT}`)
